2780ebceddeb3bd1e4ff4c593a2ebfcdcee9c5c8
조윤성 <yunsung1206@gmail.com>
2025-11-04 19:05:41 +0900

    refactor: AudioRecorder 추상화 및 ConcurrentHashMap 적용

--- src/main/java/dev/yunsung/record/AudioRecorder.java
+++ src/main/java/dev/yunsung/record/AudioRecorder.java
@@ -6,11 +6,9 @@ import java.io.File;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.TreeMap;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.sound.sampled.AudioFileFormat;
@@ -18,126 +16,73 @@ import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 
-import org.jetbrains.annotations.NotNull;
-
-import net.dv8tion.jda.api.audio.AudioReceiveHandler;
-import net.dv8tion.jda.api.audio.UserAudio;
-import net.dv8tion.jda.api.entities.channel.Channel;
-
 import dev.yunsung.stt.STT;
 
-public class AudioRecorder implements AudioReceiveHandler {
+public abstract class AudioRecorder {
 
 	static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
-	static final int RECORD_DELAY = Integer.parseInt(System.getenv("RECORD_DELAY"));
 
-	private Map<Long, ByteArrayOutputStream> audioMap; // 음성 데이터
-	private Map<Long, UserTimer> timerMap; // 유저별로 문장을 끊어 저장하기 위한 타이머
-	private Channel channel; // 음성 채널
-	private LocalDateTime dateTime; // 회의 시작 시간
-	private boolean recording = false; // 녹음 여부
+	protected final Map<Long, AudioData> bufferAudios = new ConcurrentHashMap<>();
+	protected final TreeMap<LocalDateTime, AudioData> archiveAudios = new TreeMap<>();
+
+	private String startTime;
+	private boolean recording = false;
 
 	private final STT stt;
-	private TreeMap<Long, AudioText> audioTexts;
 	private AtomicInteger activeSttJobs;
 	private final Object sttJobLock = new Object();
 
-	public AudioRecorder(STT stt) {
+	protected AudioRecorder(STT stt) {
 		this.stt = stt;
 	}
 
-	public Channel getChannel() {
-		return channel;
+	public TreeMap<LocalDateTime, AudioData> getArchiveAudios() {
+		return archiveAudios;
 	}
 
-	public TreeMap<Long, AudioText> getAudioTexts() {
-		return audioTexts;
+	protected String getStartTime() {
+		return startTime;
 	}
 
-	public String getFolderName() {
-		String dt = dateTime.format(formatter);
-		return dt + "-" + channel.getName();
-	}
+	protected abstract AudioFormat getAudioFormat();
 
-	@Override
-	public void handleUserAudio(@NotNull UserAudio userAudio) {
-		try {
-			// 말을 하고 있는 유저의 id를 가져옴
-			long userId = userAudio.getUser().getIdLong();
-			byte[] audioData = userAudio.getAudioData(1.0);
+	public abstract String getFolderName();
 
-			// 저장된 음성 데이터가 없다면 새로운 ByteArrayOutputStream을 생성
-			audioMap.computeIfAbsent(userId, k -> new ByteArrayOutputStream());
-
-			// 디스코드에서 받아온 음성 데이터 추가
-			audioMap.get(userId).write(audioData);
-
-			// 이미 사용자의 타이머가 작동 중이라면 멈추기
-			if (timerMap.get(userId) != null) {
-				timerMap.get(userId).timer().cancel();
-			}
-
-			// 타이머 추가
-			String name = userAudio.getUser().getEffectiveName();
-			Timer timer = getTimer(userId, name);
-			timerMap.put(userId, new UserTimer(name, timer));
-		} catch (Exception ignored) {
-		}
-	}
-
-	private Timer getTimer(Long userId, String name) {
-		TimerTask task = new TimerTask() {
-			public void run() {
-				// RECORD_DELAY 만큼 말을 하지 않았다면 음성 데이터를 파일로 저장
-				saveVoice(userId, name);
-			}
-		};
-		// RECORD_DELAY 후에 타이머 실행
-		Timer timer = new Timer();
-		timer.schedule(task, RECORD_DELAY);
-		return timer;
-	}
-
-	private void saveVoice(Long userId, String name) {
-		// 해당하는 사용자의 음성 데이터를 가져옴
-		ByteArrayOutputStream outputStream = this.audioMap.get(userId);
-		if (outputStream == null) {
-			return;
-		}
-
-		// 파일로 저장한 음성 데이터를 삭제
-		audioMap.remove(userId);
-		timerMap.remove(userId);
+	protected void saveVoice(AudioData audioData) {
+		// 버퍼에 있는 데이터 삭제
+		bufferAudios.remove(audioData.getId());
 
 		try {
-			long now = System.currentTimeMillis();
-
 			// wav 파일로 저장
-			saveWavFile(outputStream, now, name);
+			saveWavFile(audioData);
 
 			// 음성을 텍스트로 변환
-			transcribe(now, name);
-		} catch (IOException | InterruptedException e) {
+			transcribe(audioData);
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 	}
 
-	private void saveWavFile(ByteArrayOutputStream outputStream, long now, String name) throws IOException {
-		AudioFormat audioFormat = AudioReceiveHandler.OUTPUT_FORMAT;
-		byte[] audioData = outputStream.toByteArray();
+	private void saveWavFile(AudioData audioData) throws IOException {
+		String speaker = audioData.getSpeaker();
+		String time = audioData.getStartTime().format(formatter);
+		ByteArrayOutputStream outputStream = audioData.getAudio();
 
-		AudioInputStream ais = new AudioInputStream(new ByteArrayInputStream(audioData), audioFormat,
-			audioData.length / audioFormat.getFrameSize());
+		AudioFormat audioFormat = getAudioFormat();
+		byte[] bytes = outputStream.toByteArray();
+
+		AudioInputStream ais = new AudioInputStream(new ByteArrayInputStream(bytes), audioFormat,
+			bytes.length / audioFormat.getFrameSize());
 
 		// 음성 데이터의 길이를 구함
 		float durationInMillis = (float)(1000L * ais.getFrameLength()) / audioFormat.getFrameRate();
-		if (durationInMillis < 1000.0F) { // 길이가 1초 이상이라면 파일로 저장
+		if (durationInMillis < 1000.0F) { // 길이가 1초 미만이면 저장하지 않음
 			return;
 		}
 
-		// audio/{회의 시작 시간}-{음성 채널 이름}/{사용자명}/{현재시간.wav} 구조로 파일을 저장
-		String fileName = now + ".wav";
-		File root = new File("audio/" + getFolderName() + "/" + name);
+		// audio/{폴더 이름}/{사용자명}/{시작 시간.wav} 구조로 파일을 저장
+		String fileName = time + ".wav";
+		File root = new File("audio/" + getFolderName() + "/" + speaker);
 		File file = new File(root.getPath() + "/" + fileName);
 
 		// 폴더가 없다면 생성
@@ -148,11 +93,14 @@ public class AudioRecorder implements AudioReceiveHandler {
 		ais.close();
 	}
 
-	private void transcribe(long now, String name) throws IOException, InterruptedException {
+	private void transcribe(AudioData audioData) throws IOException, InterruptedException {
 		try {
 			activeSttJobs.incrementAndGet();
-			String sentence = stt.transcribe(getFolderName() + "/" + name + "/" + now + ".wav");
-			audioTexts.put(now, new AudioText(name, sentence));
+			String speaker = audioData.getSpeaker();
+			String time = audioData.getStartTime().format(formatter);
+			String sentence = stt.transcribe(getFolderName() + "/" + speaker + "/" + time + ".wav");
+			audioData.setSentence(sentence);
+			archiveAudios.put(audioData.getStartTime(), audioData);
 		} finally {
 			// 마지막으로 실행 중인 작업이라면 sttJobLock을 깨움
 			if (activeSttJobs.decrementAndGet() == 0) {
@@ -163,42 +111,39 @@ public class AudioRecorder implements AudioReceiveHandler {
 		}
 	}
 
-	public void startRecording(Channel channel) {
-		audioMap = new HashMap<>();
-		timerMap = new HashMap<>();
-		audioTexts = new TreeMap<>();
+	protected void startRecording() {
+		if (recording) {
+			return;
+		}
+		bufferAudios.clear();
+		archiveAudios.clear();
 		activeSttJobs = new AtomicInteger(0);
-		this.channel = channel;
-		dateTime = LocalDateTime.now();
+		startTime = LocalDateTime.now().format(formatter);
 		recording = true;
 	}
 
-	public void stopRecording() {
-		recording = false;
+	protected void stopRecording() throws InterruptedException {
+		if (!recording) {
+			return;
+		}
 
 		// 아직 저장되지 않은 음성 데이터가 있다면 저장
-		for (Long key : timerMap.keySet()) {
-			UserTimer userTimer = timerMap.get(key);
-			userTimer.timer().cancel();
-			this.saveVoice(key, userTimer.name());
+		for (Long key : bufferAudios.keySet()) {
+			AudioData audioData = bufferAudios.get(key);
+			audioData.stopTimer();
 		}
 
 		// 모든 STT 작업이 완료될 때까지 대기
-		try {
-			synchronized (sttJobLock) {
-				while (activeSttJobs.get() > 0) {
-					sttJobLock.wait();
-				}
+		synchronized (sttJobLock) {
+			while (activeSttJobs.get() > 0) {
+				sttJobLock.wait();
 			}
-		} catch (InterruptedException ignored) {
 		}
 
-		channel = null;
+		recording = false;
 	}
 
-	@Override
-	public boolean canReceiveUser() {
-		// true인 경우 디스코드로부터 음성 데이터를 받을 수 있고 false인 경우 받지 않음
+	public boolean isRecording() {
 		return recording;
 	}
 }
---
+++ src/main/java/dev/yunsung/record/DiscordAudioRecorder.java
@@ -0,0 +1,75 @@
+package dev.yunsung.record;
+
+import java.io.IOException;
+
+import javax.sound.sampled.AudioFormat;
+
+import org.jetbrains.annotations.NotNull;
+
+import net.dv8tion.jda.api.audio.AudioReceiveHandler;
+import net.dv8tion.jda.api.audio.UserAudio;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.channel.Channel;
+
+import dev.yunsung.stt.STT;
+
+public final class DiscordAudioRecorder extends AudioRecorder implements AudioReceiveHandler {
+
+	private final String name;
+	private Channel channel;
+
+	public DiscordAudioRecorder(STT stt, String name) {
+		super(stt);
+		this.name = name;
+	}
+
+	public Channel getChannel() {
+		return channel;
+	}
+
+	@Override
+	protected AudioFormat getAudioFormat() {
+		return AudioReceiveHandler.OUTPUT_FORMAT;
+	}
+
+	@Override
+	public String getFolderName() {
+		if (getChannel() == null) {
+			return null;
+		}
+		return name + "-" + getChannel().getName() + "-" + getStartTime();
+	}
+
+	@Override
+	public void handleUserAudio(@NotNull UserAudio userAudio) {
+		User user = userAudio.getUser();
+		long userId = user.getIdLong();
+		byte[] bytes = userAudio.getAudioData(1.0);
+
+		bufferAudios.computeIfAbsent(userId, k -> AudioData.discord(user, this::saveVoice));
+
+		try {
+			// 디스코드에서 받아온 음성 데이터 추가
+			bufferAudios.get(userId).write(bytes);
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public void startRecording(Channel channel) {
+		super.startRecording();
+		this.channel = channel;
+	}
+
+	@Override
+	public void stopRecording() throws InterruptedException {
+		super.stopRecording();
+		this.channel = null;
+	}
+
+	@Override
+	public boolean canReceiveUser() {
+		// true인 경우 디스코드로부터 음성 데이터를 받을 수 있고 false인 경우 받지 않음
+		return isRecording();
+	}
+}
